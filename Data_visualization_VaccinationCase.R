d995bf30b0af2b62228180a85bedf5c7d5be58f3
Kajal Srivastava <211850184+Kajal-Srivastava@users.noreply.github.com>
2025-10-09 12:26:01 +0300

    Update Data_visualization_VaccinationCase.R
    Case Study for California

--- Data_visualization_VaccinationCase.R
+++ Data_visualization_VaccinationCase.R
@@ -21,22 +21,14 @@ the_disease = "Measles"
 dat <- us_contagious_diseases |> filter(disease == the_disease & !state %in% c("Hawaii","Alaska")) |>
   mutate(rate = count / population * 10000) |> mutate(state = reorder(state, rate))
 
-#List of all the years available
-dat %>% distinct(year) %>%  arrange(year) 
-  
-#Compared state-level and national-level disease trends using geom_tile()function
-# tile plot of disease rate by state and year
-dat |> ggplot(aes(year, state, fill=rate)) +
-  geom_tile(color = "grey50") +
-  scale_x_continuous(expand = c(0,0)) +
-  scale_fill_gradientn(colors = RColorBrewer::brewer.pal(9, "Reds"), trans = "sqrt") +
-  geom_vline(xintercept = 1963, col = "green",size = 1.2) +
-  theme_minimal() + theme(panel.grid = element_blank()) +
+#Using geom_line() to see changes in California
+dat |> filter(state == "California") |> ggplot(aes(year,rate)) +
+  geom_line()+ geom_vline(xintercept=1963, col = "red",size = 1.2)+
   labs(
-    title = "📉 Impact of Measles Vaccination in the U.S",
+    title = "📉 Impact of Measles Vaccination in California",
     subtitle = "Data from Project Tycho (1928–2002) — sharp decline after vaccine enforcement in 1963",
     x = "Year",
-    y = "States in U.S",
+    y = "Total Reported Measles Cases",
     caption = "Source: University of Pittsburgh, Project Tycho"
   )
 
